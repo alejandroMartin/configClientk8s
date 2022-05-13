681fcdf6b6d2c764bd7b18c9310ba0d4aa0d0167
Alejandro Martin del Valle <Alejandro.Martinv@servexternos.gruposantander.com>
2022-05-13 09:12:26 +0200

    First commit

---
+++ src/main/java/com/example/configClientk8s/ConfigClientk8sApplication.java
@@ -0,0 +1,13 @@
+package com.example.configClientk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientk8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientk8sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/configClientk8s/ConfigClientk8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configClientk8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientk8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
